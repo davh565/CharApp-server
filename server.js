ab54f3603dd29e7673d9c455806599c585413280
David Humphries <d.a.humphries@gmail.comname>
2019-01-05 17:43:21 +1300

    initial commit server

---
+++ server.js
@@ -0,0 +1,57 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+
+
+mongoose.connect(
+    "mongodb://davh:briadir32dh!@CGTest-shard-00-00-h0zai.mongodb.net:27017,cgtest-shard-00-01-h0zai.mongodb.net:27017,cgtest-shard-00-02-h0zai.mongodb.net:27017/CGTest?ssl=true&replicaSet=CGTest-shard-0&authSource=admin&retryWrites=true"
+  );
+
+  var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("DB connected")
+});
+
+var msgSchema = new mongoose.Schema({
+    message: String
+});
+var Message = mongoose.model('Message',msgSchema);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
+  io.on('connection',function(socket){
+      console.log('a user connected');
+      socket.on('disconnect', function(){
+        console.log('user disconnected');
+      });
+      Message.find(function(err,messages){
+          if(err) return console.log('Retrieval Error!');
+          for(const message of messages){
+            io.emit('chat message', message);
+          }
+      })
+
+      socket.on('chat message', function(msg){
+
+          var message = new Message({
+              message: msg
+          });
+          console.log(message)
+          message.save(function(err, message){
+              if(err){
+                  console.log('Save Error!');
+              }
+          })
+
+
+        io.emit('chat message', message);
+        console.log('message: ' + msg);
+      });
+  });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
